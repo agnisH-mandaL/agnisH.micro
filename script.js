4a22a19ba16db1658b75259be5b0e108bd18a116
agnisH-mandaL <agnishmandal.2005@gmail.com>
2025-03-29 21:10:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var theme=document.getElementById("theme");
+theme.onclick=function()
+{
+    document.body.classList.toggle("dark-theme");
+theme.on
+}
+var sidebtn=document.getElementById("sidebtn");
+sidebtn.onclick=function()
+{
+    document.body.classList.toggle("sidebtnon");
+sidebtn.on
+}
