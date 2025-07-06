6f37ba8370600c9beddc530b535ff78d19bba76b
agnisH-mandaL <agnishmandal.2005@gmail.com>
2025-07-06 15:26:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var theme=document.getElementById("theme");
+window.onload=function(){
+    initTheme();
+};
+function initTheme()
+{
+    const theme=localStorage.getItem('theme');
+    if (theme=="dark-theme")
+    {
+        document.body.classList.toggle("dark-theme");
+    theme.on
+    }
+    
+}
 theme.onclick=function()
 {
-    document.body.classList.toggle("dark-theme");
-theme.on
+    localStorage.getItem('theme')==='dark-theme'?localStorage.setItem('theme',''):localStorage.setItem('theme','dark-theme');
+    initTheme();
+
 }
 var sidebtn=document.getElementById("sidebtn");
 sidebtn.onclick=function()
